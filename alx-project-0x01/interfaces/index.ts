2c925a7cd283244f44fae9d84acab44053338f72
Kawona <ojekawona@gmail.com>
2025-09-19 15:21:40 +0000

    Update index.ts

--- alx-project-0x01/interfaces/index.ts
+++ alx-project-0x01/interfaces/index.ts
@@ -1,6 +1,25 @@
-export interface PostProps {
+export interface UserProps {
   userId: number;
   id: number;
+  name: string;
   title: string;
   body: string;
+   email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    };
+  };
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
 }
