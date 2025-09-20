6a9096a09774138d6c05522875da62048bf6b61a
Kawona <ojekaona@gmail.com>
2025-09-20 07:48:34 +0000

    Updated file

--- components/layout/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Footer: React.FC = () => (
-    <footer className="bg-gray-100 text-center text-gray-600 py-4 mt-8">
-        {new Date().getFullYear()} my App. All rights reserved.
-    </footer>
-);
-
-export default Footer;
